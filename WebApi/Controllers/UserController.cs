acb4737ced8c6e8d81954dde0b79e91210a8e753
Alexander <zubkov_95@mail.ru>
2022-03-20 16:15:30 +0500

    remove FromBody from delete action

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using WebApi.Data;
+using WebApi.Data.Entities;
 
 namespace WebApi.Controllers;
 
@@ -28,7 +29,7 @@ public class UserControllerContext : BaseControllerContext<User, Guid>
     }
     
     [HttpDelete]
-    public async Task<IActionResult> Delete([FromBody]Guid id)
+    public async Task<IActionResult> Delete(Guid id)
     {
         try
         {
